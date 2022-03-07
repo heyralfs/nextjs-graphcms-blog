66ebf1f3f64b00e03ec34cad7b5e23e2360c569d
Ralf Oliveira Ferreira <ralfoliveira@gmail.com>
2022-03-07 19:08:21 -0300

    Basic setup with Chakra] UI

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { Flex, Text } from "@chakra-ui/react";
+
+export default function Home() {
+	return (
+		<Flex w="100%" h="100vh" align="center" justify="center">
+			<Text fontSize="2xl">Hello world</Text>
+		</Flex>
+	);
+}
